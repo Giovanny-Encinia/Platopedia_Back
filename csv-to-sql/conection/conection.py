dbc02dacedbb3b6a864fd37e7484dac34d0f93d5
Engio <giovannyencinia@gmail.com>
2020-08-01 00:21:10 -0500

    Se crean tablas en mysql, falata llenarlas

--- csv-to-sql/conection/conection.py
+++ csv-to-sql/conection/conection.py
@@ -1,12 +1,11 @@
-def conect_mysql():
+def conect_mysql(user="root", host="127.0.01", password=":PA2PA6BY9::gio&{zai}"):
     """
     This function makes a conection with a mysql database
+    We can change the user, host, and password
     """
     import mysql.connector
 
-    database = mysql.connector.connect(
-        user="root", host="127.0.0.1", password=":PA2PA6BY9::gio&{zai}"
-    )
+    database = mysql.connector.connect(user=user, host=host, password=password)
     cursor = database.cursor(buffered=True)
     return (database, cursor)
 
